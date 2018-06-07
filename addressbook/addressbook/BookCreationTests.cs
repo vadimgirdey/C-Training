a034c7be5a7ae4dd19078a0523f516a325b7755c
vadim <vgirdey@gmail.com>
2018-06-06 21:07:46 -0500

    Added new test
    Creation new book contact

---
+++ addressbook/addressbook/BookCreationTests.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressBookTests
+{
+    [TestFixture]
+    public class BookCreationTestc
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheCreationUserTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToAddNewPage();
+
+            FillNewContact fill = new FillNewContact("firstName", "lastName");
+
+            //driver.FindElement(By.Name("firstname")).Clear();
+            //driver.FindElement(By.Name("firstname")).SendKeys("FirstName");
+            //driver.FindElement(By.Name("lastname")).Clear();
+            //driver.FindElement(By.Name("lastname")).SendKeys("LastName");
+            //driver.FindElement(By.Name("nickname")).Clear();
+            //driver.FindElement(By.Name("nickname")).SendKeys("Nickname");
+            //driver.FindElement(By.Name("company")).Clear();
+            //driver.FindElement(By.Name("company")).SendKeys("Test Company, Inc");
+            // ERROR: Caught exception [Error: Dom locators are not implemented yet!]
+            LogOut();
+        }
+
+        private void LogOut()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void GoToAddNewPage()
+        {
+            driver.FindElement(By.XPath("//a[text()= 'add new']")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/addressbook/index.php");
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ addressbook/addressbook/FillNewContact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace WebAddressBookTests
+{
+    public class FillNewContact
+    {
+        private string firstName;
+        private string lastName;
+
+        public FillNewContact(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+}
